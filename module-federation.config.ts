63c6da15c178fc385746a0028755fbba56924ad7
Hien Pham <hienminhpham@fortna.com>
2025-07-26 23:45:55 +0700

    update module federation config to include shared dependencies for vue and mfe-ui-kit

--- module-federation.config.ts
+++ module-federation.config.ts
@@ -1,10 +1,13 @@
 import { createModuleFederationConfig } from '@module-federation/enhanced/rspack';
 
 export default createModuleFederationConfig({
-    name: 'product',
-    exposes: {
-        './HelloWorld': './src/components/main.ts',
-    },
-    shared: {},
-    dts: false
+  name: 'product',
+  exposes: {
+    './HelloWorld': './src/components/main.ts',
+  },
+  shared: {
+    vue: { singleton: true, eager: true, requiredVersion: '^3.5.17' },
+    'mfe-ui-kit': { singleton: true, eager: true, requiredVersion: false },
+  },
+  dts: false,
 });
